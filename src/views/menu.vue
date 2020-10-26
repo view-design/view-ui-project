053cf4523cdbcb065072ba8215bdddfce8d02191
Li Xiang <119146715@qq.com>
2020-10-26 08:13:39 +0800

    dev init

--- src/views/menu.vue
+++ src/views/menu.vue
@@ -1,5 +1,5 @@
 <template><div>
-        <Menu :theme="theme3" active-name="1">
+        <Menu theme="dark" active-name="1">
         <MenuGroup title="内容管理">
             <MenuItem name="1">
                 <Icon type="md-document" />
@@ -23,19 +23,19 @@
     </Menu>
     <br>
     <p>Change theme</p>
-    <RadioGroup v-model="theme3">
-        <Radio label="light"></Radio>
-        <Radio label="dark"></Radio>
-    </RadioGroup>
+    <!--<RadioGroup v-model="theme3">-->
+        <!--<Radio label="light"></Radio>-->
+        <!--<Radio label="dark"></Radio>-->
+    <!--</RadioGroup>-->
 
  </div>
 </template>
 <script>
     export default {
-        data () {
-            return {
-                theme3: 'light'
-            }
-        }
+//        data () {
+//            return {
+//                theme3: 'light'
+//            }
+//        }
     }
 </script>
